6bb5536cf9ed8b8ca8b10d9588f92219b588db52
SeiichiN <billie175@gmail.com>
2026-09-16 14:49:18 +0900

    jqueryに変える added.

---
+++ jQueryに変える/5-02_location/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+$(function () {
+  const lang = $('html').attr('lang');
+
+  switch (lang) {
+    case 'ja':
+      $('option[value="index.html"]').prop('selected', true);
+      break;
+    case 'en':
+      $('option[value="index-en.html"]').prop('selected', true);
+      break;
+    case 'zh':
+      $('option[value="index-zh.html"]').prop('selected', true);
+      break;
+  }
+
+  $('#form select').on('change', function() {
+    location.href = $(this).val();
+  });
+});
